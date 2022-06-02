95e73d5a2bf14973f9059ad74576cfb37c836b50
Radivoj Gibarac <rgibarac@gmail.com>
2022-06-02 18:18:23 +0200

    task-3

---
+++ app/code/BeeIT/HelloWorld/Controller/Index/Hello.php
@@ -0,0 +1,29 @@
+<?php
+namespace BeeIT\HelloWorld\Controller\Index;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\HTTP\PhpEnvironment\Request;
+use Magento\Customer\Model\Session;
+
+class Hello extends \Magento\Framework\App\Action\Action
+{
+    protected $_pageFactory;
+    protected $customerSession;
+
+    public function __construct(Context $context, Session $customerSession)
+    {
+        $this->customerSession = $customerSession;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $customerID = $this->customerSession->getCustomerId();
+        $result->setContents( "Hello customer with the id of $customerID.");
+        return $result;
+
+    }
+}
