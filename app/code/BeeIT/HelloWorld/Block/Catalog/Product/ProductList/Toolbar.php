ec119e963cb380a16a33c75719b246284592600d
Radivoj Gibarac <Rgibarac@gmail.com>
2022-06-20 13:19:10 +0200

    Cleaned code

--- app/code/BeeIT/HelloWorld/Block/Catalog/Product/ProductList/Toolbar.php
+++ app/code/BeeIT/HelloWorld/Block/Catalog/Product/ProductList/Toolbar.php
@@ -4,22 +4,12 @@ namespace BeeIT\HelloWorld\Block\Catalog\Product\ProductList;
 
 class Toolbar extends \Magento\Catalog\Block\Product\ProductList\Toolbar
 {
-    /**
-     * Retrieve available Order fields list
-     *
-     * @return array
-     */
     public function getAvailableOrders()
     {
         $this->loadAvailableOrders();
         return $this->_availableOrder;
     }
-
-    /**
-     * Load Available Orders
-     *
-     * @return \Magento\Catalog\Block\Product\ProductList\Toolbar
-     */
+    
     private function loadAvailableOrders()
     {
         if ($this->_availableOrder === null) {
