01c0035704bbd25654b178422f757beb6051b64a
Radivoj Gibarac <Rgibarac@gmail.com>
2022-06-20 13:42:49 +0200

    Added plugin Best Sellers Plugin as an option for sorting products

---
+++ app/code/BeeIT/HelloWorld/Plugin/AddBestSellersToToolbarAvailableOrders.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BeeIT\HelloWorld\Plugin;
+
+use Magento\Catalog\Block\Product\ProductList\Toolbar;
+
+class AddBestSellersToToolbarAvailableOrders
+{
+    public function aroundGetAvailableOrders(Toolbar $subject, callable $proceed)
+    {
+        $result = $proceed();
+        $result['best sellers - plugin'] = 'Best Sellers - Plugin';
+        return $result;
+
+    }
+}
